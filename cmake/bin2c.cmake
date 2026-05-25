fb76fbd6525591eeaa3eb7c5a71ec12180dd274b
vxjnc <vxjnc0@gmail.com>
2026-05-25 19:15:45 +0400

    feat: в bin2c и embedded_resources добавлен режим для генерации std::string_view

--- cmake/bin2c.cmake
+++ cmake/bin2c.cmake
@@ -1,7 +1,17 @@
-file(READ ${INPUT_FILE} HEX_CONTENT HEX)
+if(TEXT_MODE)
+    file(READ ${INPUT_FILE} FILE_CONTENT)
 
-string(REPEAT "[0-9a-f][0-9a-f]" 12 REGEX_PATTERN)
-string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " C_ARRAY ${HEX_CONTENT})
+    string(REPLACE "\\" "\\\\" FILE_CONTENT "${FILE_CONTENT}")
+    string(REPLACE "\"" "\\\"" FILE_CONTENT "${FILE_CONTENT}")
+    string(REPLACE "\n" "\\n\"\n\"" FILE_CONTENT "${FILE_CONTENT}")
 
-file(WRITE ${OUTPUT_FILE} "#include <cstdint>\n\n")
-file(APPEND ${OUTPUT_FILE} "const uint8_t ${VAR_NAME}[] = {\n${C_ARRAY}\n};\n")
+    file(WRITE ${OUTPUT_FILE} "#pragma once\n#include <string_view>\n\n")
+    file(APPEND ${OUTPUT_FILE} "constexpr std::string_view ${VAR_NAME} =\n\"${FILE_CONTENT}\";\n")
+else()
+    file(READ ${INPUT_FILE} HEX_CONTENT HEX)
+
+    string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " C_ARRAY ${HEX_CONTENT})
+
+    file(WRITE ${OUTPUT_FILE} "#pragma once\n#include <cstdint>\n\n")
+    file(APPEND ${OUTPUT_FILE} "const uint8_t ${VAR_NAME}[] = {\n${C_ARRAY}\n};\n")
+endif()
--- cmake/embedded_resources.cmake
+++ cmake/embedded_resources.cmake
@@ -1,4 +1,9 @@
 function(embed_bin_resource INPUT_FILE OUTPUT_DIR VAR_PREFIX)
+    set(TEXT_MODE ${ARGV3})
+    if(TEXT_MODE)
+        set(TEXT_MODE_ARG "-DTEXT_MODE=1")
+    endif()
+
     get_filename_component(RAW_NAME ${INPUT_FILE} NAME_WE)
 
     string(REPLACE " " "_" SAFE_NAME "${RAW_NAME}")
@@ -16,6 +21,7 @@ function(embed_bin_resource INPUT_FILE OUTPUT_DIR VAR_PREFIX)
         COMMAND ${CMAKE_COMMAND} -DINPUT_FILE=${INPUT_FILE} 
                                  -DOUTPUT_FILE=${H_FILE} 
                                  -DVAR_NAME=${ARR_NAME} 
+                                 ${TEXT_MODE_ARG}
                                  -P "${CMAKE_CURRENT_SOURCE_DIR}/cmake/bin2c.cmake"
         DEPENDS ${INPUT_FILE} "${CMAKE_CURRENT_SOURCE_DIR}/cmake/bin2c.cmake"
         COMMENT "Embedding resource: ${RAW_NAME}"
@@ -26,9 +32,13 @@ function(embed_bin_resource INPUT_FILE OUTPUT_DIR VAR_PREFIX)
 endfunction()
 
 file(GLOB FONT_FILES "${CMAKE_SOURCE_DIR}/GUI/interface/font_manager/fonts/*.ttf" "${CMAKE_SOURCE_DIR}/GUI/interface/font_manager/fonts/*.otf")
-
 foreach(FONT ${FONT_FILES})
     embed_bin_resource(${FONT} "${GENERATED_DIR}/fonts" "font")
+endforeach()
+
+file(GLOB TRANSLATE_FILES "${CMAKE_SOURCE_DIR}/assets/translations/*.lang")
+foreach(TRANSLATE ${TRANSLATE_FILES})
+    embed_bin_resource(${TRANSLATE} "${GENERATED_DIR}/translate" "translate" 1)
 endforeach()
 
 add_custom_target(embedded_resources DEPENDS ${G_RES_HEADERS})
